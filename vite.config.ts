554b473a2acb83cfae260eae87ff52b70d399b6b
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-10-02 22:38:23 +0200

    Fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
@@ -9,41 +8,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': join(__dirname, './src'),
-=======
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueI18n from "@intlify/vite-plugin-vue-i18n";
-
-import { join, resolve } from "path";
-export default defineConfig({
-  mode: "development",
-  resolve: {
-    alias: {
-      "@": join(__dirname, "./src"),
->>>>>>> main
     },
   },
   plugins: [
     vue(),
     vueI18n({
-<<<<<<< HEAD
       include: resolve(__dirname, './locales/**'),
     }),
   ],
   define: { 'process.env': {} },
-=======
-      include: resolve(__dirname, "./locales/**"),
-    }),
-  ],
-  define: { "process.env": {} },
->>>>>>> main
   css: {
     preprocessorOptions: {
       scss: { additionalData: ` @import "@/styles/variables.scss";` },
     },
   },
-<<<<<<< HEAD
 })
-=======
-});
->>>>>>> main
