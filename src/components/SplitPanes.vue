24aae695ceba2149d37b510a42a5e0df85e31b87
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-10-04 11:57:33 +0200

    Dependancies

---
+++ src/components/SplitPanes.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="split-container">
+    <splitpanes class="default-theme" @resize="Resizing">
+      <pane :size="side">
+        <slot name="left"></slot>
+      </pane>
+      <pane>
+        <slot name="right"></slot>
+      </pane>
+    </splitpanes>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Splitpanes, Pane } from 'splitpanes'
+  import 'splitpanes/dist/splitpanes.css'
+  export default {
+    name: 'SplitPane',
+    components: { Splitpanes, Pane },
+    props: {
+      side: { type: Number, default: 50 },
+      onResize: {
+        type: Function,
+        default: (): void => {
+          return
+        },
+      },
+    },
+    methods: {
+      Resizing(event): void {
+        this.paneSize = event[0].size
+        this.onResize()
+      },
+    },
+  }
+</script>
+<style lang="scss">
+  .split-container {
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+  }
+</style>
