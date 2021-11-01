9bc0f1d980ae19307412bd048ed2d913cbc5f43d
Maël <mlhoutel@gmail.com>
2021-11-01 15:32:13 +0100

    feat: :sparkles: Add link to github

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     @nada webapp is a free and open-source app for the analysis of
-    meteorological data
+    meteorological data.
+    <a href="https://github.com/IMT-Atlantique-FIL-2020-2023/NADA-webapp"
+      >browse on github</a
+    >
   </div>
 </template>
 <script lang="ts">
